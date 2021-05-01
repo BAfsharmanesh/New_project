c5d228abb574b9671248269762938e1bc5f49dbe
Bahram Afsharmanesh <75673997+BAfsharmanesh@users.noreply.github.com>
2021-05-01 16:05:46 +0430

    Update regression_metrics.py

--- src/regression_metrics.py
+++ src/regression_metrics.py
@@ -1,4 +1,6 @@
 # the metric used in indoor-location-navigation competition
+import numpy as np
+
 def iln_comp_metric(xhat, yhat, fhat, x, y, f):
     intermediate = np.sqrt(np.power(xhat - x,2) + np.power(yhat-y,2)) + 15 * np.abs(fhat-f)
     return intermediate.sum()/xhat.shape[0]
