3ca2735d92f6ca968d455b08030c7bf021e81049
Bahram Afsharmanesh <75673997+BAfsharmanesh@users.noreply.github.com>
2021-05-01 12:14:20 +0430

    Update cross_validation.py

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -81,7 +81,7 @@ class CrossValidation:
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("../input/train_multilabel.csv")
+    df = pd.read_csv("../input/indoor-navigation-and-location-wifi-features/wifi_features/train/5a0546857ecc773753327266_1000_train.csv")
     cv = CrossValidation(df, shuffle=True, target_cols=["attribute_ids"], 
                          problem_type="multi_col_regression", num_folds=5)
     df_split = cv.split()
